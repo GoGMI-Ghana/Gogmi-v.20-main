b014fe99ebb1990beaa2ac399861a4ede78af08d
GoGMI-Ghana <info@gogmi.org.gh>
2026-03-05 21:43:15 +0000

    Update Login.jsx

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -45,12 +45,12 @@ const Login = () => {
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-black text-[#132552]">
-          Sign in to your account
+          MEMBERSHIP
         </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
           Or{' '}
           <Link to="/register" className="font-semibold text-[#8E3400] hover:text-[#6B2700]">
-            create a new account
+            Sign in to you account
           </Link>
         </p>
       </div>
