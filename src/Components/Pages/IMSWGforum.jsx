52aa46ece0ee02502ba58ea8e9fd0f3b5f55cc2a
GoGMI-Ghana <info@gogmi.org.gh>
2026-03-12 10:37:57 +0000

    Update IMSWGforum.jsx

--- src/Components/Pages/IMSWGforum.jsx
+++ src/Components/Pages/IMSWGforum.jsx
@@ -60,7 +60,7 @@ const IMSWGforum = () => {
     setErrors({});
     
     try {
-      const apiUrl = 'https://gogmi.org.gh/api/api/imswg-forum.php';
+      const apiUrl = 'https://api.gogmi.org.gh/api';
       const response = await fetch(`${apiUrl}/imswg-forum.php`, {
         method: 'POST',
         headers: {
